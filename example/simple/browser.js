699f1300f3a56ce2dcf2f42f640bebddb0b7f2fa
James Halliday <mail@substack.net>
2014-11-05 00:24:31 -0800

    convert paths to point arrays

--- example/simple/browser.js
+++ example/simple/browser.js
@@ -1,6 +1,7 @@
 var linearize = require('svg-linearize');
 var loadsvg = require('load-svg');
 var parse = require('parse-svg-path');
+var toinform = require('../../');
 
 var code = document.querySelector('#code');
 
@@ -9,9 +10,18 @@ loadsvg('face.svg', function (err, svg) {
     document.body.insertBefore(nsvg, code);
     
     var paths = nsvg.querySelectorAll('path');
+    var points = [];
+    
     for (var i = 0; i < paths.length; i++) {
-        var p = paths[i];
-        var pts = parse(p.getAttribute('d'));
-        console.log(JSON.stringify(pts));
+        var cmds = parse(paths[i].getAttribute('d'));
+        var pts = [];
+        for (var j = 0; j < cmds.length; j++) {
+            var c = cmds[j];
+            if (c[0] === 'M' || c[0] === 'L') {
+                pts.push(c.slice(1));
+            }
+        }
+        points.push(pts);
     }
+    code.value = toinform(points);
 });
